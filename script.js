07f0e993f6c69f3c48a6d240c4b1d3f5984c0c15
alter412 <139802395+alter412@users.noreply.github.com>
2024-02-19 14:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //your JS code here. If required.
+var modal = document.getElementsByClassName("modal")[0];
 var openBtn = document.getElementById("openModal");
-openBtn.onClick = function () {
+var closeBtn = document.getElementsByClassName("close-modal")[0];
+openBtn.onclick = function () {
 	modal.style.display="block";
 }
+
+closeBtn.onclick = function () {
+	modal.style.display="none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
